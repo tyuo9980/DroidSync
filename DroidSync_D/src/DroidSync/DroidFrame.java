2a26ad885217007bb0093ccf8d6d25e5cf966986
Julie Xue <j23xue@uwaterloo.ca>
2014-08-06 22:32:00 -0400

    add "add device" button

--- DroidSync_D/src/DroidSync/DroidFrame.java
+++ DroidSync_D/src/DroidSync/DroidFrame.java
@@ -1,12 +1,12 @@
 package DroidSync;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Label;
 import java.awt.Panel;
-import java.awt.Rectangle;
 import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
@@ -14,8 +14,10 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.util.Vector;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 public class DroidFrame  implements ActionListener
 {
@@ -37,6 +39,10 @@ public class DroidFrame  implements ActionListener
 	public Graphics g;
 	public BufferedImage image;
 	public Canvas canvas;
+	public JComboBox<String> dropdown;
+	public Vector<String> deviceList;
+	public JOptionPane addDeviceWindow;
+	public JButton addDeviceButton;
 
 	public DroidFrame()
 	{
@@ -63,38 +69,51 @@ public class DroidFrame  implements ActionListener
 		//title
 		title = new Label("Hello!!",Label.CENTER);
 		
+		//button
+		addDeviceButton = new JButton("Add Device");
+		addDeviceButton.addActionListener(new ActionListener() {
+ 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+            	displayAddDevice();
+            }
+        }); 
+		//String typeDevice = JOptionPane.showInputDialog(f,"Enter device's type: ","Add Device",JOptionPane.PLAIN_MESSAGE);
+	//	String nameDevice = JOptionPane.showInputDialog(f,"Enter device's name: ","Add Device",JOptionPane.PLAIN_MESSAGE);
+		//DroidSync.addDevice(typeDevice,nameDevice);
+		//dropdown = new JComboBox(deviceList);
+		dropdown = new JComboBox();
 		//table
-		Vector<Vector> deviceList = new Vector<Vector>();
-		
+		deviceList = new Vector<String>();
 		for(int i=0;i<DroidSync.list.size();i++){
-			Vector<String> device = new Vector<String>();
-			device.addElement(DroidSync.list.elementAt(i).type);
-			device.addElement(DroidSync.list.elementAt(i).name);
-			deviceList.addElement(device);
+			dropdown.addItem(DroidSync.list.elementAt(i).type+"-"+DroidSync.list.elementAt(i).name);
+			//deviceList.add(DroidSync.list.elementAt(i).type+"-"+DroidSync.list.elementAt(i).name);
 		}
-		Vector<String> columnNames = new Vector<String>();
-		columnNames.addElement("Type");
-	    columnNames.addElement("Name");
-	    JTable table = new JTable(deviceList, columnNames);
-	    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-	    table.getColumnModel().getColumn(0).setPreferredWidth(100);
-	    table.getColumnModel().getColumn(1).setPreferredWidth(100);
-		JScrollPane scrollPane = new JScrollPane(table);
-		table.setPreferredScrollableViewportSize(table.getPreferredSize());
 		
+		
+		//add device window
+		
+		//JOptionPane.showMessageDialog(f,"Enter the name's device: ","Add Device",JOptionPane.PLAIN_MESSAGE);
 		// panel;
 		p = new Panel();
 		p.setLayout(new BorderLayout());
 		p.add(title, BorderLayout.PAGE_START);
 		p.add(textArea, BorderLayout.EAST);
-		p.add(scrollPane, BorderLayout.WEST);
+		p.add(dropdown, BorderLayout.WEST);
 		p.add(textField, BorderLayout.PAGE_END);
+		p.add(addDeviceButton, BorderLayout.CENTER);
 		textField.addActionListener(this);
 		f.getContentPane().add(p);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+	public void displayAddDevice(){
+		String typeDevice = JOptionPane.showInputDialog(f,"Enter device's type: ","Add Device",JOptionPane.PLAIN_MESSAGE);
+		String nameDevice = JOptionPane.showInputDialog(f,"Enter device's name: ","Add Device",JOptionPane.PLAIN_MESSAGE);
+		DroidSync.addDevice(typeDevice,nameDevice);
+		dropdown.addItem(typeDevice+"-"+nameDevice);
+	}
 	public void actionPerformed(ActionEvent evt) {
 	    String text = textField.getText();
 	    textArea.append(text + "\n");
--- DroidSync_D/src/DroidSync/DroidSync.java
+++ DroidSync_D/src/DroidSync/DroidSync.java
@@ -5,29 +5,36 @@ import java.util.Vector;
 public class DroidSync
 {	
 	public static Vector<Device> list;
+	public static void addDevice(String type, String name){
+		list.add(new Device(type,name));
+	}
+	public static void addDevice(Device d){
+		list.add(d);
+	}
 	public static void main(String args[])
 	{	
 		list = new Vector<Device>();
-		list.add(new Device("Phone","Julie1"));
-		list.add(new Device("Phone","Julie2"));
-		list.add(new Device("Tablet","Julie3"));
-		list.add(new Device("Tablet","Julie4"));
-		list.add(new Device("Phone","Julie5"));
-		list.add(new Device("Phone","Julie6"));
-		list.add(new Device("Phone","Julie7"));
-		list.add(new Device("Tablet","Julie8"));
-		list.add(new Device("Tablet","Julie9"));
-		list.add(new Device("Phone","Julie10"));
-		list.add(new Device("Phone","Julie11"));
-		list.add(new Device("Phone","Julie12"));
-		list.add(new Device("Tablet","Julie13"));
-		list.add(new Device("Tablet","Julie14"));
-		list.add(new Device("Phone","Julie15"));
-		list.add(new Device("Phone","Julie16"));
-		list.add(new Device("Phone","Julie17"));
-		list.add(new Device("Tablet","Julie18"));
-		list.add(new Device("Tablet","Julie19"));
-		list.add(new Device("Phone","Julie20"));
+		addDevice("Phone","Julie1");
+		addDevice("Phone","Julie2");
+		addDevice("Tablet","Julie3");
+		addDevice("Tablet","Julie4");
+		addDevice("Phone","Julie5");
+		addDevice("Phone","Julie6");
+		addDevice("Phone","Julie7");
+		addDevice("Tablet","Julie8");
+		addDevice("Tablet","Julie9");
+		addDevice("Phone","Julie10");
+		addDevice("Phone","Julie11");
+		addDevice("Phone","Julie12");
+		addDevice("Tablet","Julie13");
+		addDevice("Tablet","Julie14");
+		addDevice("Phone","Julie15");
+		addDevice("Phone","Julie16");
+		addDevice("Phone","Julie17");
+		addDevice("Tablet","Julie18");
+		addDevice("Tablet","Julie19");
+		addDevice("Phone","Julie20");
+
 		
 		DroidFrame f = new DroidFrame();
 	}
